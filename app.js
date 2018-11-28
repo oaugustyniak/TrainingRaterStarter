e43a1e1af90901a89536e96573cb22565e848824
Oscar Augustyniak <oaugustyniak@milestechnologies.com>
2018-11-28 16:05:00 -0500

    Sess5 redo

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ require('./config/config');
 const models = require('./models');
 require('./global_functions');
 const sessions = require('./controllers/SessionsController');
-const bodyParser = require('body-parser');
-const app = express();
-
-const express = require('express');
-require('./config/config');
-const models = require('./models');
-require('./global_functions');
-const sessions = require('./controllers/SessionsController');
 const users = require('./controllers/UsersController');
 const bodyParser = require('body-parser');
 const app = express();
@@ -19,12 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // CORS
-app.use(function ( req, res, next ){
-    // website you will allow to connect
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // pass to next layer of middleware
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 
+      'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
     next();
-})
+});
 
 app.get('/', (req, res) => { res.send( 'Hello World!!' )} );
 
