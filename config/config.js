8f4dd166a5f6c78028b84b0099aacc85799c706b
oaugustyniak <43833702+oaugustyniak@users.noreply.github.com>
2018-12-16 23:54:46 -0500

    missed from branch

--- config/config.js
+++ config/config.js
@@ -5,3 +5,6 @@ CONFIG.db_dialect = 'mysql';
 CONFIG.db_name = 'TrainingRaterStarter';
 CONFIG.db_user = 'root';
 CONFIG.db_password = 'miles';
+CONFIG.db_password = 'password';
+CONFIG.jwt_encryption = '1jk3pozcrzxejhqsnaz6q0u1';
+CONFIG.jwt_expiration = '1000';
